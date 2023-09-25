465c7e288f31cb513b110391998cb392e2e6b80a
Amauri Cocom <amaurihely1342@gmail.com>
2023-09-25 11:28:13 -0600

    Axios Test and Navigate to /Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -22,8 +22,9 @@ function App() {
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/Login" element={<Login />} />
             <Route path="/Home" element={<Home />} />
+            <Route path="/" element={<Navigate to="/Home" />} />
           </Routes>
         </div>
       )}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,20 @@
 import { Typography, Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 class Login extends React.Component {
+  
+  state = {
+    users: []
+  }
+
+  async componentDidMount(){
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+    this.setState({users:res.data})
+    console.log('usuarios', this.state.users)
+  }
+
   render() {
     return (
       <div>
